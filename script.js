2a3219f9ce8ecb105349d9558dde53e98965d4bd
Adeleke <lekea4@gmail.com>
2021-04-15 17:14:20 +0100

    basic website by oge

---
+++ script.js
@@ -0,0 +1,45 @@
+AOS.init();
+window.onload = function () {
+  window.addEventListener("scroll", function (e) {
+    if (window.pageYOffset > 100) {
+      document.querySelector("header").classList.add("is-scrolling");
+    } else {
+      document.querySelector("header").classList.remove("is-scrolling");
+    }
+  });
+
+  const menu_btn = document.querySelector(".hamburger");
+  const mobile_menu = document.querySelector(".mobile-nav");
+
+  menu_btn.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+};
+
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  this.classList.add("active");
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+}
+
+function removeShow() {
+  tabContentItems.forEach((item) => {
+    item.classList.remove("show");
+  });
+}
+
+tabItems.forEach((item) => {
+  item.addEventListener("click", selectItem);
+});
